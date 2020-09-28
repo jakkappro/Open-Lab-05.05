ae3dd963a28e6ef0fbc4b4b14ca12471077f546d
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-09-28 20:25:25 +0200

    I made function to change string to alternating caps string

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -6,7 +6,25 @@ namespace Open_Lab_05._05
     {
         public string AlternatingCaps(string original)
         {
-            throw new NotImplementedException();
+            string ret = "";
+            int index = 0;
+            for(int i = 0; i < original.Length; i++)
+            {
+                if (original.ToCharArray()[i].ToString() != " ")
+                {
+                    if (index % 2 == 0)
+                        ret += original.ToCharArray()[i].ToString().ToUpper();
+                    else
+                        ret += original.ToCharArray()[i].ToString().ToLower();
+
+                    index++;
+                }
+                else
+                {
+                    ret += " ";
+                }
+            }
+            return ret;
         }
     }
 }
